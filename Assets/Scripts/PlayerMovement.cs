beaa62e876d8c6bebb663a32aa33201904b11275
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2018-01-29 00:00:44 -0200

    Add the game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public Rigidbody rb;
+
+	public float velocidade = 600f;
+	public float sidewaysForce = 500f;
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+		rb.AddForce (0, 0, velocidade * Time.deltaTime);
+
+		if ( Input.GetKey("d"))
+		{
+
+			rb.AddForce(sidewaysForce* Time.deltaTime, 0,0);
+
+		}
+	
+		if ( Input.GetKey("a"))
+		{
+
+			rb.AddForce(-sidewaysForce* Time.deltaTime, 0,0);
+
+		}
+
+		if (rb.position.y < -1f)
+			FindObjectOfType<GameManager> ().EndGame ();
+	}
+
+
+}
---
+++ Assets/Scripts/colider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class colider : MonoBehaviour {
+
+	public PlayerMovement movement;
+
+	void OnCollisionEnter (Collision infocolisao)
+	{
+		if (infocolisao.collider.tag == "Obstacle")
+			
+		{
+
+			movement.enabled = false;
+			FindObjectOfType<GameManager>().EndGame ();
+		}
+
+
+
+	}
+
+}
